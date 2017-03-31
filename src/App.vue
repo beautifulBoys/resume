3c6c1de182ffb6279b298d5baf2db90208e095c1
beautifulBoys <1298947916@qq.com>
2017-03-31 16:46:08 +0800

    基础代码

---
+++ src/App.vue
@@ -0,0 +1,487 @@
+<template>
+  <div class="main-box">
+    <span class="topTip">别拉了，到顶啦！</span>
+    <span class="bottomTip">别拉了，到底啦！</span>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide bg1">
+          <div class="page-main">
+            <div class="left">
+              <h3>Target :</h3>
+              <p>对绚丽的网页十分热爱，对优秀的前端交互十分向往，对前端技术痴迷！</p>
+            </div>
+            <div class="right">
+              <img src="./images/a25.png">
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide bg2">
+          <div class="page-main">
+            <div class="left">
+              <h3>Personal details :</h3>
+              <ul>
+                <li>毕业院校：沈阳化工大学</li>
+                <li>学　　历：大学本科</li>
+                <li>专　　业：工商管理</li>
+                <li>出生年月：1993-08-30</li>
+                <li>现居住地：辽宁 沈阳</li>
+              </ul>
+              <ul>
+                <li>工作状态：在职</li>
+                <li>工作经验：一年</li>
+                <li>工作地点：北京</li>
+                <li>联系电话：13352478028</li>
+                <li>电子邮箱：1298947916@qq.com</li>
+              </ul>
+            </div>
+            <div class="right">
+              <img src="./images/lixin.png">
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide bg3">
+          <div class="page-main">
+            <h3>Skill & Assessment :</h3>
+            <ul>
+              <li>
+                <p> 使用<span class="code">HTML5</span>和<span class="code">CSS3</span>快速编写符合W3c标准web页面，了解 HTML5
+                  中对网页标签的语义强化，熟练使用 CSS3 编写网页动画和效果。</p>
+              </li>
+              <li>
+                <p> 熟练使用<span class="code">HTML5</span>开发适应不同分辨率手机的流式布局网页。熟练使用<span class="code">Bootstrap</span>做响应式开发。
+                </p>
+              </li>
+              <li>
+                <p> 常用的JavaScript库有<span class="code">jQuery.js</span>，<span class="code">Vue.js</span>，<span
+                  class="code">Knockout.js</span>。擅长使用<span class="code">Vue.js</span>全套技术栈快速开发。熟悉<span class="code">Node.js</span>，可以作简单的后端开发。
+                </p>
+              </li>
+              <li>
+                <p> 熟悉组件化开发，在实际项目中参与过部分组件的设计，编写以及后期维护；了解<span class="code">Hybrid</span>开发模式。</p>
+              </li>
+              <li>
+                <p> 热爱钻研新的技术和标准，熟悉<span class="code">ES6</span>，常用<span class="code">LESS</span>作为CSS预处理语言，熟悉<span
+                  class="code">NPM</span>和<span class="code">Webpack</span>，版本控制工具用<span class="code">SVN</span>。</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="swiper-slide bg4">
+          <div class="page-main">
+            <h3>Education :</h3>
+            <ul>
+              <li>
+                <h5>2012年09月 --- 2016年07月</h5>
+                <p><span class="kong">沈阳化工大学</span>|<span class="kong">工商管理</span>|<span class="kong">本科</span>|<span
+                  class="kong">统招</span></p>
+              </li>
+              <li>
+                <h5>2008年09月 --- 2012年06月</h5>
+                <p><span class="kong">古浪一中</span>|<span class="kong">高中-理科</span>|<span class="kong">高中</span>|<span
+                  class="kong">统招</span></p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="swiper-slide bg5">
+          <div class="page-main">
+            <h3>Project :</h3>
+            <ul>
+              <li>
+                <h5>门店销售App（Hybrid）</h5>
+                <p><span class="title">项目介绍：</span>
+                  该项目是安卓App。由公司下设门店员工线下销售使用。该项目用Cordova打包，安卓内嵌HTML的方式实现。用模块化的方式分离HTML、CSS 和
+                  JavaScript代码，实现按需加载。用knockout实现数据层的实时更新。用Ajax实现前后端交互。</p>
+                <p><span class="title">工作职责：</span>
+                  负责前端展示页面模板的编写及后期维护。业务需求的分析、配合后端及产品制定业务流程。业务逻辑的编写及修改BUG等完善工作。后期为适应新的业务需求，主导过一次改版，并优化用户体验及大幅提升性能。</p>
+              </li>
+              <li>
+                <h5>企业管理系统ERP</h5>
+                <p><span class="title">项目介绍：</span> 该项目是Web App应用。由总公司业务管理使用。前端用
+                  VUE整套技术栈，组件化开发，组件采用BootStrap样式。用Ajax实现前后端交互。</p>
+                <p><span class="title">工作职责：</span> 负责部分组件的编写和后期维护。负责业务需求的理解和部分业务逻辑的编写。负责和后端的沟通。修复项目BUG。</p>
+              </li>
+              <li>
+                <h5>档口采购App（Hybrid）</h5>
+                <p><span class="title">项目介绍：</span> 该项目是安卓App应用。由公司下设档口采购员采购使用。该项目用Cordova打包，安卓内嵌HTML的方式实现。前端用
+                  VUE整套技术栈，组件化开发。用Ajax实现前后端交互。</p>
+                <p><span class="title">工作职责：</span> 负责项目前期的前端架构。负责业务需求的理解和部分业务逻辑的编写。负责和后端的沟通，把握流程和处理问题。</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="swiper-slide bg6">
+          <div class="page-main">
+            <h3>写在最后</h3>
+            <div class="evaluation">
+              <p>
+                有一年前端开发经验，熟练掌握Vue.js技术栈，能独立进行PC端以及手机端开发。为人真诚可靠，对工作认真负责，在一年的工作中得到全公司所
+                有员工的一致认可。个性乐观执着，积极向上，敢于面对困难与挑战，向往接受新生事物及技术，决心在未来成为一个前端大牛。具有较强的学习能
+                力和团队合作能力，能够快速融入团队开始工作。
+              </p>
+            </div>
+            <div class="tell-me">
+              <span class="span icon1" title="邮箱" @click="clickEvent(1)"></span>
+              <span class="span icon2" title="QQ" @click="clickEvent(2)"></span>
+              <span class="span icon3" title="GitHub" @click="clickEvent(3)"></span>
+              <span class="span icon4" title="个人网站" @click="clickEvent(4)"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    mounted () {
+      new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        speed: 700,
+        keyboardControl: true,
+        mousewheelControl: true,
+        direction: 'vertical'
+      });
+    },
+    methods: {
+      clickEvent (n) {
+        if (n === 1) window.open('http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=X25tZmdma2hmbmkfLi5xPDAy');
+        if (n === 2) window.open('http://wpa.qq.com/msgrd?v=3&uin=1298947916');
+        if (n === 3) window.open('https://github.com/beautifulBoys');
+        if (n === 4) window.open('https://beautifulBoys.github.io');
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import '../lib/swiper/swiper.css';
+
+  .code {
+    padding: 4px 8px;
+    margin: 0 3px;
+    background: #eee;
+    border-radius: 3px;
+  }
+
+  .ulStyle {
+    width: 1000px;
+    background: #fff;
+    padding: 30px;
+    h3 {
+      font-size: 20px;
+      font-weight: normal;
+      margin: 20px 0;
+      line-height: 40px;
+      color: #333;
+      font-family: "arial";
+      text-indent: 20px;
+    }
+    ul {
+      width: 70%;
+      li {
+        list-style-type: decimal;
+        p {
+          font-size: 15px;
+          color: #444;
+          line-height: 35px;
+        }
+      }
+    }
+  }
+
+  .absoluteCenter {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .main-box {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    font-family: "Microsoft YaHei";
+    top: 0;
+    .topTip {
+      text-align: center;
+      line-height: 80px;
+      width: 100%;
+      display: block;
+    }
+    .bottomTip {
+      text-align: center;
+      line-height: 80px;
+      width: 100%;
+      display: block;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+    .swiper-slide {
+      &.bg1 {
+        background: #dedede;
+        .absoluteCenter();
+        .page-main {
+          width: 1000px;
+          height: 250px;
+          background: #fff;
+          padding: 30px;
+          display: flex;
+          .left {
+            flex: 1;
+            padding: 0 30px 0 0;
+            h3 {
+              font-size: 20px;
+              font-weight: normal;
+              margin: 20px 0;
+              line-height: 40px;
+              color: #333;
+              font-family: "arial";
+            }
+            p {
+              font-size: 15px;
+              color: #444;
+              text-indent: 30px;
+              line-height: 40px;
+            }
+          }
+          .right {
+            width: 400px;
+            img {
+              width: 100%;
+              height: 100%;
+              border-radius: 1px;
+              opacity: 0.9;
+            }
+          }
+        }
+      }
+      &.bg2 {
+        background: #ccc;
+        .absoluteCenter();
+        .page-main {
+          height: 300px;
+          width: 1000px;
+          background: #fff;
+          padding: 30px;
+          display: flex;
+          .left {
+            flex: 1;
+            padding: 0 30px 0 0;
+            h3 {
+              font-size: 20px;
+              font-weight: normal;
+              margin: 20px 0;
+              line-height: 40px;
+              color: #333;
+              font-family: "arial";
+              text-indent: 20px;
+            }
+            ul {
+              width: 260px;
+              padding-left: 20px;
+              display: inline-block;
+              margin: 0;
+              li {
+                list-style-type: none;
+                font-size: 15px;
+                color: #444;
+                line-height: 35px;
+              }
+            }
+          }
+          .right {
+            width: 230px;
+            img {
+              width: 100%;
+              height: 100%;
+              border-radius: 1px;
+              border: 1px solid #eee;
+            }
+          }
+        }
+      }
+      &.bg3 {
+        background: #dedede;
+        .absoluteCenter();
+        .page-main {
+          height: 500px;
+          .ulStyle();
+        }
+      }
+      &.bg4 {
+        background: #ccc;
+        .absoluteCenter();
+        .page-main {
+          height: 340px;
+          width: 1000px;
+          background: #fff;
+          padding: 30px;
+          h3 {
+            font-size: 20px;
+            font-weight: normal;
+            margin: 20px 0;
+            line-height: 40px;
+            color: #333;
+            font-family: "arial";
+            text-indent: 20px;
+          }
+          ul {
+            width: 70%;
+            li {
+              list-style-type: decimal;
+              border-bottom: 1px dashed #aaa;
+              p {
+                font-size: 15px;
+                color: #444;
+                line-height: 35px;
+                .kong {
+                  padding: 0 40px;
+                }
+              }
+              h5 {
+                font-size: 14px;
+              }
+            }
+          }
+        }
+      }
+      &.bg5 {
+        background: #dedede;
+        .absoluteCenter();
+        .page-main {
+          height: 600px;
+          width: 1000px;
+          background: #fff;
+          padding: 30px;
+          h3 {
+            font-size: 20px;
+            font-weight: normal;
+            margin: 20px 0;
+            line-height: 40px;
+            color: #333;
+            font-family: "arial";
+            text-indent: 20px;
+          }
+          ul {
+            width: 90%;
+            li {
+              list-style-type: decimal;
+              border-bottom: 1px dashed #aaa;
+              p {
+                font-size: 14px;
+                color: #444;
+                line-height: 28px;
+                margin: 12px 0;
+                .title {
+                  font-weight: 600;
+                }
+              }
+              h5 {
+                font-size: 14px;
+                margin: 12px 0;
+              }
+            }
+          }
+        }
+      }
+      &.bg6 {
+        background: #ccc;
+        .absoluteCenter();
+        .page-main {
+          height: 320px;
+          width: 1000px;
+          background: #fff;
+          padding: 30px;
+          h3 {
+            font-size: 20px;
+            font-weight: normal;
+            margin: 20px 0;
+            line-height: 40px;
+            color: #333;
+            text-align: center;
+            font-family: "arial";
+          }
+          .evaluation {
+            width: 100%;
+            height: 150px;
+            padding: 0 50px;
+            box-sizing: border-box;
+            p {
+              font-size: 15px;
+              color: #333;
+              line-height: 35px;
+              text-indent: 30px;
+            }
+          }
+          .tell-me {
+            width: 300px;
+            margin: 0 auto;
+            .span {
+              width: 50px;
+              height: 50px;
+              margin: 0 10px;
+              border-radius: 100%;
+              display: inline-block;
+              cursor: pointer;
+              &.icon1 {
+                background: #666 url(./images/email.svg) no-repeat center center;
+                background-size: 30px;
+                &:hover {
+                  background: #00c600 url(./images/email.svg) no-repeat center center;
+                  background-size: 33px;
+                }
+              }
+              &.icon2 {
+                background: #666 url(./images/qq.svg) no-repeat center center;
+                background-size: 27px;
+                &:hover {
+                  background: #e8571b url(./images/qq.svg) no-repeat center center;
+                  background-size: 30px;
+                }
+              }
+              &.icon3 {
+                background: #666 url(./images/github.svg) no-repeat center center;
+                background-size: 27px;
+                &:hover {
+                  background: #ffb80c url(./images/github.svg) no-repeat center center;
+                  background-size: 30px;
+                }
+              }
+              &.icon4 {
+                background: #666 url(./images/site.svg) no-repeat center center;
+                background-size: 27px;
+                &:hover {
+                  background: #0089e8 url(./images/site.svg) no-repeat center center;
+                  background-size: 30px;
+                }
+              }
+            }
+
+          }
+        }
+      }
+    }
+    .swiper-container {
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+</style>
+<style lang="less">
+  .swiper-pagination {
+    margin-right: 10px;
+    .swiper-pagination-bullet {
+      width: 10px;
+      height: 10px;
+      margin: 10px 0 !important;
+    }
+  }
+</style>
